fd085013609d2eed572386e0297a0ef2931a97cc
wansongtao <18101837209@163.com>
2024-10-18 10:16:19 +0800

    feat: 使用ConfigModule读取环境文件并设为全局模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+      isGlobal: true,
+      cache: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

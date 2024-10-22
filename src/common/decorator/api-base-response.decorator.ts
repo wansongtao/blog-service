2f37758701a5a4bea234e2fefe937776cf4c4892
wansongtao <18101837209@163.com>
2024-10-22 14:08:51 +0800

    feat(decorator): 封装设置 swagger 响应类型的装饰器

---
+++ src/common/decorator/api-base-response.decorator.ts
@@ -0,0 +1,39 @@
+import { applyDecorators, Type } from '@nestjs/common';
+import { ApiExtraModels, ApiOkResponse, getSchemaPath } from '@nestjs/swagger';
+import {
+  BaseResponseEntity,
+  NullResponseEntity,
+} from '../entities/base-response.entity';
+
+export const ApiBaseResponse = <TModel extends Type<any>>(
+  model?: TModel,
+  type: 'string' | 'array' | 'object' = 'object',
+) => {
+  if (!model) {
+    return ApiOkResponse({ type: NullResponseEntity });
+  }
+
+  return applyDecorators(
+    ApiExtraModels(BaseResponseEntity, model),
+    ApiOkResponse({
+      schema: {
+        allOf: [
+          { $ref: getSchemaPath(BaseResponseEntity) },
+          {
+            properties: {
+              data: {
+                type: type,
+                $ref:
+                  type === 'object' && model ? getSchemaPath(model) : undefined,
+                items:
+                  type === 'array' && model
+                    ? { $ref: getSchemaPath(model) }
+                    : undefined,
+              },
+            },
+          },
+        ],
+      },
+    }),
+  );
+};

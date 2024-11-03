d657b02032bf4102a75fb664770abf5793ae5710
wansongtao <18101837209@163.com>
2024-11-03 22:25:20 +0800

    feat(auth): 注册 jwt 模块

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
+import { JwtModule, type JwtModuleOptions } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { getBaseConfig } from 'src/common/config';
+import { join } from 'path';
+import { readFileSync } from 'fs';
 
 @Module({
+  imports: [
+    UserModule,
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => {
+        const config = getBaseConfig(configService);
+        const staticPath = join(__dirname, '../../../');
+        const privateKey = readFileSync(
+          join(staticPath, config.jwt.privateKeyPath),
+        );
+        const publicKey = readFileSync(
+          join(staticPath, config.jwt.publicKeyPath),
+        );
+
+        const options: JwtModuleOptions = {
+          privateKey,
+          publicKey,
+        };
+
+        return options;
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })

ef7a65f54fd9b3515f7126b219d7cb90db2fbd3a
wansongtao <18101837209@163.com>
2024-11-03 22:27:22 +0800

    feat(auth): 登录接口

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Ip, Headers } from '@nestjs/common';
+import { Controller, Get, Ip, Headers, Body, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiOperation } from '@nestjs/swagger';
 import { ApiBaseResponse } from 'src/common/decorator/api-base-response.decorator';
-import { AuthEntity } from './entities/auth.entity';
+import { AuthEntity, LoginEntity } from './entities/auth.entity';
+import { LoginDto } from './dto/auth.dto';
+import { BaseResponseEntity } from 'src/common/entities/base-response.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -18,5 +20,18 @@ export class AuthController {
     @Headers('user-agent') userAgent: string,
   ): AuthEntity {
     return this.authService.generateCaptcha(ip, userAgent);
+  }
+
+  @ApiOperation({
+    summary: '登录',
+  })
+  @ApiBaseResponse(LoginEntity)
+  @Post('login')
+  login(
+    @Body() data: LoginDto,
+    @Ip() ip: string,
+    @Headers('user-agent') userAgent: string,
+  ): Promise<LoginEntity | BaseResponseEntity> {
+    return this.authService.login(data, ip, userAgent);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { create as createCaptcha } from 'svg-captcha';
 import { InjectRedis } from '@nestjs-modules/ioredis';
 import Redis from 'ioredis';
 import { ConfigService } from '@nestjs/config';
 import { getBaseConfig } from 'src/common/config';
-import { getCaptchaKey } from 'src/common/config/redis-key';
+import { getCaptchaKey, getSSOKey } from 'src/common/config/redis-key';
+import { LoginDto } from './dto/auth.dto';
+import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
+
+import type { IPayload } from 'src/common/types';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRedis() private readonly redis: Redis,
     private readonly configService: ConfigService,
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
   ) {}
 
   generateCaptcha(ip: string, userAgent: string) {
@@ -29,5 +36,59 @@ export class AuthService {
     return {
       captcha: `data:image/svg+xml;base64,${Buffer.from(captcha.data).toString('base64')}`,
     };
+  }
+
+  async validateCaptcha(ip: string, userAgent: string, captcha: string) {
+    const key = getCaptchaKey(ip, userAgent);
+    const value = await this.redis.get(key);
+
+    if (value && value.toLowerCase() === captcha.toLowerCase()) {
+      this.redis.del(key);
+      return true;
+    }
+
+    return false;
+  }
+
+  async login(data: LoginDto, ip: string, userAgent: string) {
+    const isCaptchaValid = await this.validateCaptcha(
+      ip,
+      userAgent,
+      data.captcha,
+    );
+    if (!isCaptchaValid) {
+      return {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: '验证码错误',
+      };
+    }
+
+    const isUserValid = await this.userService.validateUser(
+      data.userName,
+      data.password,
+    );
+    if (!isUserValid) {
+      return {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: '用户名或密码错误',
+      };
+    }
+
+    const config = getBaseConfig(this.configService);
+    const algorithm = config.jwt.algorithm;
+    const payload: IPayload = { userName: data.userName };
+    const token = this.jwtService.sign(payload, {
+      algorithm,
+      expiresIn: config.jwt.expiresIn,
+    });
+    const refreshToken = this.jwtService.sign(payload, {
+      algorithm,
+      expiresIn: config.jwt.refreshTokenIn,
+    });
+
+    const ssoKey = getSSOKey(data.userName);
+    this.redis.set(ssoKey, token, 'EX', config.jwt.expiresIn);
+
+    return { token, refreshToken };
   }
 }

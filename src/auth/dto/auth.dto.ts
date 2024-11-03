622527bfe5af987ac4678358379dca3c147c395a
wansongtao <18101837209@163.com>
2024-11-03 22:22:59 +0800

    feat(auth): 添加登录接口参数校验 dto 文件

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, Matches } from 'class-validator';
+
+export class LoginDto {
+  @Matches(/^[a-zA-Z][a-zA-Z0-9]{2,10}$/, { message: '用户名格式错误' })
+  @IsNotEmpty({ message: '用户名不能为空' })
+  @ApiProperty({ description: '用户名' })
+  userName: string;
+
+  @Matches(/^[a-zA-Z](?=.*[.?!&_])(?=.*\d)[a-zA-Z\d.?!&_]{5,15}$/, {
+    message: '密码格式错误',
+  })
+  @IsNotEmpty({ message: '密码不能为空' })
+  @ApiProperty({ description: '密码' })
+  password: string;
+
+  @Matches(/^[a-zA-Z0-9]{4}$/, { message: '验证码格式错误' })
+  @IsNotEmpty({ message: '验证码不能为空' })
+  @ApiProperty({ description: '验证码' })
+  captcha: string;
+}

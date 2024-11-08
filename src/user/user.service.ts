9386ead5fe04d9f89599dff6a7201dcbd1b20a60
wansongtao <18101837209@163.com>
2024-11-08 15:13:39 +0800

    feat(user): 将验证用户方法接受的密码参数改为可选

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,13 +6,17 @@ import bcrypt from 'bcrypt';
 export class UserService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async validateUser(userName: string, password: string) {
+  async validateUser(userName: string, password?: string) {
     const user = await this.prismaService.user.findUnique({
       where: { userName, deleted: false, disabled: false },
       select: { id: true, password: true },
     });
     if (!user) {
       return false;
+    }
+
+    if (password === undefined) {
+      return true;
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);

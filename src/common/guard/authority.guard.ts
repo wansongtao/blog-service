5373417101cc7bf01891ea6936e42904afd51dd6
wansongtao <18101837209@163.com>
2024-12-17 20:20:00 +0800

    feat(guard): 添加权限守卫

---
+++ src/common/guard/authority.guard.ts
@@ -0,0 +1,44 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AUTHORITY_KEY } from '../decorator/authority.decorator';
+import { RedisService } from 'src/redis/redis.service';
+import { ConfigService } from '@nestjs/config';
+import { getBaseConfig } from 'src/common/config';
+
+import { IPayload } from '../types';
+
+@Injectable()
+export class AuthorityGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly configService: ConfigService,
+    private readonly redisService: RedisService,
+  ) {}
+
+  async canActivate(context: ExecutionContext) {
+    const needPermissions = this.reflector.get<string[]>(
+      AUTHORITY_KEY,
+      context.getHandler(),
+    );
+    if (!needPermissions) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user: IPayload = request.user;
+
+    const { defaultAdmin } = getBaseConfig(this.configService);
+    if (user.userName === defaultAdmin.username) {
+      return true;
+    }
+
+    const permissions = await this.redisService.getUserPermission(user.userId);
+    if (
+      permissions.some((permission) => needPermissions.includes(permission))
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+}

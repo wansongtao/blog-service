8dfe97f935f4fb32eb35ca3c869c3cb7abf6d111
wansongtao <18101837209@163.com>
2025-01-20 14:27:04 +0800

    perf(upload): 优化上传模块的swagger文档

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,10 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UploadService } from './upload.service';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ApiBaseResponse } from 'src/common/decorator/api-base-response.decorator';
 import { PresignedDto } from './dto/presigned.dto';
 import { PresignedEntity } from './entities/presigned.entity';
 
+@ApiTags('upload')
+@ApiBearerAuth()
 @Controller('upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}

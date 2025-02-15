2d3ceed2aab1fe5a5acac1419ec097baf31b5e3f
wansongtao <18101837209@163.com>
2025-02-15 10:22:36 +0800

    perf(role): 优化查询角色接口的 dto 对象

--- src/role/dto/query-role.dto.ts
+++ src/role/dto/query-role.dto.ts
@@ -1,21 +1,20 @@
 import { BaseQueryDto } from '../../common/dto/base-query.dto';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsOptional, Matches, MaxLength } from 'class-validator';
+import { IsOptional, Matches, MaxLength } from 'class-validator';
 import { Transform } from 'class-transformer';
 
 export class QueryRoleDto extends BaseQueryDto {
-  @IsBoolean()
   @Transform(({ value }) => value === '1')
   @IsOptional()
-  @ApiProperty({ required: false, description: '是否禁用' })
+  @ApiProperty({ required: false, description: '禁用状态' })
   disabled?: boolean;
 
   @MaxLength(50)
-  @Matches(/^[a-zA-Z\u4e00-\u9fa5,_-]*$/, { message: '名称格式错误' })
+  @Matches(/^[a-zA-Z\u4e00-\u9fa5,_-]*$/, { message: '角色名称格式错误' })
   @IsOptional()
   @ApiProperty({
     required: false,
-    description: '名称关键字, 不能超过50个字符',
+    description: '角色名称关键字, 不能超过50个字符',
   })
   keyword?: string;
 }

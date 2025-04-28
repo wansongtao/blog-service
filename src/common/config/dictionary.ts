56bad7c07068396d5e439d9cbb3a360f38e851c6
wansongtao <18101837209@163.com>
2025-04-28 15:29:00 +0800

    feat(common): 修改文章可见字典类型

--- src/common/config/dictionary.ts
+++ src/common/config/dictionary.ts
@@ -1,8 +1,4 @@
 /**
  * PRIVATE - 仅自己可见，INTERNAL - 仅后台可见，PUBLIC - 公开
  */
-export const ARTICLE_VISIBILITY = {
-  PRIVATE: 1,
-  INTERNAL: 2,
-  PUBLIC: 3,
-} as const;
+export const ARTICLE_VISIBILITY = ['PRIVATE', 'INTERNAL', 'PUBLIC'] as const;
